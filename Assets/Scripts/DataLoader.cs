6664bf25f085d0dc3031de16b623fd0d0b3d77b8
Игорь Квасников <kv4snikov@gmail.com>
2021-05-24 08:41:30 +0300

    update scenes

--- Assets/Scripts/DataLoader.cs
+++ Assets/Scripts/DataLoader.cs
@@ -6,6 +6,12 @@ public class DataLoader : MonoBehaviour
     void Start()
     {
         PlayerController.playerInventory = BetweenScenesData.PlayerInventory;
+        StartCoroutine(UpdateUi());
+    }
+
+    IEnumerator UpdateUi()
+    {
+        yield return new WaitForSeconds(0.1f);
         PlayerController.UpdateUI();
     }
 }
